e2c75904cf9020e73dc4f95c60cb132322361f80
skynette <cutejosh2@gmail.com>
2020-04-21 02:53:24 +0100

    I hate this

--- day1.py
+++ day1.py
@@ -3,6 +3,6 @@ def twofer(name):
     # Enter your code here. Read input from STDIN. Print output to STDOUT
 
 	if name:
-		return ("One for {}, and one for me.".format(name))
+		return ("One for {}, one for me.".format(name))
 	else:
-		return ("One for you, and one for me.")
+		return ("One for you, one for me.")
