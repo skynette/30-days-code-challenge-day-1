0c691cc4f40b69b7be2f4bb67725f284cda061e3
skynette <cutejosh2@gmail.com>
2020-04-21 13:25:36 +0100

    Update day1.py

--- day1.py
+++ day1.py
@@ -2,6 +2,6 @@
 def twofer(name):
     # Enter your code here. Read input from STDIN. Print output to STDOUT
 	if name.isalpha():
-		return ("One for "+name+", one for me.")
+		return("One for "+name+", one for me.")
 	else:
 		return("One for you, one for me.")
