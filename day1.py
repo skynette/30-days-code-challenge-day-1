1d2e078c8164caefae108a8cc7b5707506858495
L0n3w4lf <cutejosh2@gmail.com>
2020-04-21 21:36:32 +0100

    im so tired

--- day1.py
+++ day1.py
@@ -1,7 +1,7 @@
 
 def twofer(name="you"):
     # Enter your code here. Read input from STDIN. Print output to STDOUT
-    if name.isalpha():
+    if name:
         return("One for {}, one for me.".format(name))
     else:
         return("One for you, one for me.")
