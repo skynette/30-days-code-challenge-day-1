67c9e5430b9c71af2eec9b4f0acb6e82260f8da0
skynette <cutejosh2@gmail.com>
2020-04-21 02:50:33 +0100

    final commmit

--- day1.py
+++ day1.py
@@ -1,7 +1,8 @@
 
 def twofer(name):
     # Enter your code here. Read input from STDIN. Print output to STDOUT
-    if name:
-	return ("One for {}, and one for me.".format(name))
-    else:
-	return("One for you, and one for me.")
+
+	if name:
+		return ("One for {}, and one for me.".format(name))
+	else:
+		return ("One for you, and one for me.")
